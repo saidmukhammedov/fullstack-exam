7c62a638abd6747c7a5aca1d358bbffc2e87ea8e
saidmukhammedov <asaidmukhammedov008@gmail.com>
2026-05-25 22:01:29 +0500

    app.js updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/task.routes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 dotenv.config();
 
@@ -13,10 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-console.log("MONGO_URI:", process.env.MONGO_URI);
-
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
+app.use(errorHandler);
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -24,9 +23,6 @@ mongoose
   .catch((err) => console.log("DB error:", err));
 
 const PORT = 5000;
-
-import { errorHandler } from "./middlewares/errorHandler.js";
-app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
